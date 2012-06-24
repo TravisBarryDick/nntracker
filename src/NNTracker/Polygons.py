a378952f6fc366bf06c28fe1098560f150fd2913
Travis Dick <travis.barry.dick@gmail.com>
2012-06-24 14:33:58 -0600

    Added comments to Polygons.py

--- src/NNTracker/Polygons.py
+++ src/NNTracker/Polygons.py
@@ -1,6 +1,23 @@
+"""
+Some tools for computing various properties of polygons
+Author: Travis Dick (travis.barry.dick@gmail.com)
+"""
+
 import numpy as np
 
 def polygon_descriptors(corners):
+    """ Computes the area, perimeter, and center of mass of a polygon.
+    
+    Parameters:
+    -----------
+    corners : (2,n) numpy array
+      The vertices of the polygon. Should be in clockwise or
+      counter-clockwise order.
+    
+    Returns:
+    --------
+    A tuple (perimeter, area, (center of mass x, center of mass y)).
+    """
     n_points = corners.shape[1]
     p, a, cx, cy = 0, 0, 0, 0
     for i in xrange(n_points):
