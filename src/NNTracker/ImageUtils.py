2ae2598beb8c16d065091cd1a07bbb5257cc1ae5
Travis Dick <travis.barry.dick@gmail.com>
2012-06-24 14:16:11 -0600

    Added comments to ImageUtils.py

--- src/NNTracker/ImageUtils.py
+++ src/NNTracker/ImageUtils.py
@@ -1,3 +1,8 @@
+"""
+Some tools for efficiently manipulating images.
+Author: Travis Dick (travis.barry.dick@gmail.com)
+"""
+
 import itertools
 
 import cv2
@@ -8,6 +13,26 @@ from scipy.weave import converters
 from Homography import *
 
 def draw_region(img, corners, color, thickness=1):
+    """ Draws a warped rectangle in an image.
+
+    Parameters:
+    -----------
+    img : (n,m) numpy array
+      The image to be drawn into
+      
+    corners : (2,4) numpy array
+      An array whose columns are the corners of the warped
+      rectangle. Points should be in (counter-)clockwise order.
+
+    color: scalar or (b,g,r) triple
+      The color to make the region. In black and white images
+      a scalar between 0 and 1 specifies intensity, and in 
+      color images, a (b,g,r) triple (0 <= b,g,r <= 255) specifies
+      the color.
+
+    thickness: integer
+      The width in pixels of the lines used.
+    """
     for i in xrange(4):
         p1 = (int(corners[0,i]), int(corners[1,i]))
         p2 = (int(corners[0,(i+1)%4]), int(corners[1,(i+1)%4]))
@@ -18,6 +43,18 @@ def draw_region(img, corners, color, thickness=1):
         cv2.line(img, p1, p2, color, thickness)
 
 def to_grayscale(img):
+    """ Converts an bgr8 image into a grayscale image.
+
+    Parameters:
+    -----------
+    img : (n,m,3) numpy array
+      The input image, with 3 channels. Entries should take
+      values in the range (0,256).
+    
+    Returns:
+    --------
+    An (n,m) numpy array with entries between 0 and 1.
+    """
     (height, width, depth) = img.shape
     grayscale = np.empty((height,width), dtype=np.float64)
     code = \
@@ -36,6 +73,32 @@ def to_grayscale(img):
     return grayscale
 
 def sample_region(img, pts, result=None):
+    """ Samples the image intenisty at a collection of points.
+
+    Notes: 
+    ------
+      - Only works with grayscale images. 
+      - All points outside the bounds of the image have intensity 0.5.
+
+    Parameters:
+    -----------
+    img : (n,m) numpy array
+      The image to be sampled from.
+     
+    pts : (2,k) numpy array
+      The points to be sampled out of the image. These may be sub-pixel
+      coordinates, in which case bilinear interpolation is used.
+
+    result : (k) numpy array (optional)
+      Optionally you can pass in a results vector which will store the 
+      sampled vector. If you do not supply one, this function will allocate
+      one and return a reference.
+    
+    Returns:
+    --------
+    Returns a (k) numpy array containing the intensities of the given 
+    sub-pixel coordinates in the provided image.
+    """
     num_pts = pts.shape[1]
     (height, width) = img.shape
     if result == None: result = np.empty(num_pts)
@@ -71,6 +134,16 @@ def sample_region(img, pts, result=None):
     return result
 
 def sample_and_normalize(img, pts):
+    """ Samples the image intensity at a collection of points 
+    and normalizes the result.
+
+    Identical to sample_region, except the result is shifted
+    so that it's components have mean 0.
+
+    See Also:
+    ---------
+    sample_region
+    """
     result = sample_region(img, pts);
     result -= result.mean()
     return result
